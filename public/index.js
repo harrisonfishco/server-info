b3301e64fa6b9df9c35a7750ab9597f7aa11d054
Harrisonfishco <harrisonfishco@gmail.com>
2022-02-24 21:35:55 -0700

    Fixed Memory Data

--- public/index.js
+++ public/index.js
@@ -6,6 +6,6 @@ fetch('/info', {
 }).then(response => response.json()).then(data => {
     console.log(data.mem)
     document.querySelector('#CPU-NAME').innerText = data.cpus[0].model
-    document.querySelector('#MEM-USAGE').innerText = ((data.fmem / data.tmem) * 100).toFixed(2) + "%"
-    document.querySelector('#MEM-INFO').innerText = (data.fmem / (1000000000.0)).toFixed(2) + " GB / " + (data.tmem / (1000000000.0)).toFixed(2) + " GB"
+    document.querySelector('#MEM-USAGE').innerText = (((data.tmem - data.fmem) / data.tmem) * 100).toFixed(2) + "%"
+    document.querySelector('#MEM-INFO').innerText = ((data.tmem - data.fmem) / (1000000000.0)).toFixed(2) + " GB / " + (data.tmem / (1000000000.0)).toFixed(2) + " GB"
 })
