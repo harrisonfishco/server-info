3e7173ed873baf1df8fa23b2ec1de0d838c90699
Harrisonfishco <harrisonfishco@gmail.com>
2022-02-28 11:43:08 -0700

    Added System Info Interval

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
-fetch('/info', {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}).then(response => response.json()).then(data => {
-    console.log(data.mem)
-    document.querySelector('#CPU-NAME').innerText = data.cpus[0].model
-    document.querySelector('#MEM-USAGE').innerText = (((data.tmem - data.fmem) / data.tmem) * 100).toFixed(2) + "%"
-    document.querySelector('#MEM-INFO').innerText = ((data.tmem - data.fmem) / (1000000000.0)).toFixed(2) + " GB / " + (data.tmem / (1000000000.0)).toFixed(2) + " GB"
-})
+const fetchSysInfo = () => {
+    fetch('/info', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json()).then(data => {
+        document.querySelector('#CPU-NAME').innerText = data.cpus[0].model
+        document.querySelector('#MEM-USAGE').innerText = (((data.tmem - data.fmem) / data.tmem) * 100).toFixed(2) + "%"
+        document.querySelector('#MEM-INFO').innerText = ((data.tmem - data.fmem) / (1000000000.0)).toFixed(2) + " GB / " + (data.tmem / (1000000000.0)).toFixed(2) + " GB"
+    })
+}
+
+const getSys = setInterval(fetchSysInfo, 1000)
