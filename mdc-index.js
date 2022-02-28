56adfb2f08bac92e63851c082b082824eeb4d218
Harrisonfishco <harrisonfishco@gmail.com>
2022-02-28 11:11:36 -0700

    Added Dev Banner

--- mdc-index.js
+++ mdc-index.js
@@ -1,3 +1,7 @@
 import { MDCBanner } from "@material/banner";
+import { MDCRipple } from "@material/ripple/index";
+
+new MDCRipple(document.querySelector('.mdc-button.mdc-banner__primary-action'))
 
 const banner = new MDCBanner(document.querySelector('.mdc-banner'))
+banner.open()
